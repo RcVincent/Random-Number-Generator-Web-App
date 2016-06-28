62df6c66e8c40a1bb7d705beefceb33d2f9e1436
Ryan <rvincent@ycp.edu>
2016-06-27 21:26:17 -0400

    assorted work

--- src/DBpersist/IDatabase.java
+++ src/DBpersist/IDatabase.java
@@ -8,5 +8,7 @@ public interface IDatabase  {
 	 List<User> addUserToDatabse(final String name, final String pass, final String email, final String type, final String firstN, final String lastN);
 	 List<User> matchUsernameWithPassword(String name);
 	 List<User> DeleteUserFromDatabase(String username, String password);
+	 List<User> changeUserName(final String username, final String newUserName, final String password);
+	
 	
 }
--- src/vincent/ryan/c/RNG/servlet/ChangeAccountInfoServlet.java
+++ src/vincent/ryan/c/RNG/servlet/ChangeAccountInfoServlet.java
@@ -1,5 +1,73 @@
 package vincent.ryan.c.RNG.servlet;
 
-public class ChangeAccountInfoServlet {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vincent.ryan.c.RNG.controller.matchUsernameWithPassword;
+
+
+public class ChangeAccountInfoServlet {
+	private static final long serialVersionUID = 1L;
+	private matchUsernameWithPassword match = null;
+	private ChangeUsername change = null;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String user = (String) req.getSession().getAttribute("username");
+		if (user == null) {
+			// user is not logged in, or the session expired
+			resp.sendRedirect(req.getContextPath() + "/Login");
+			return;
+		}
+		req.getRequestDispatcher("/_view/ChangeUsername.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		// Decode form parameters and dispatch to controller
+		String errorMessage = null;
+		String username = null;
+		String newUsername = null;
+		String password = null;
+		
+		//get parameters from the jsp
+		username = req.getParameter("username");
+		newUsername = req.getParameter("newUsername");
+		password = req.getParameter("password");
+		
+		//Validate login credentials
+		match = new matchUsernameWithPassword();
+		ArrayList<User> user = null;
+		user = match.matchUser(username);
+		if(user.size()>0){
+			User u = user.get(0);
+			ProjectController controller = new ProjectController();
+			
+			//if user is authenticated, call change password
+			if(controller.authenticate(u, password)){
+				change = new ChangeUsername();
+				change.changeUsername(username, newUsername, password);
+				
+				//set session username to new username
+				req.getSession().setAttribute("username", newUsername);
+				errorMessage = "New username was successfully created";
+				req.setAttribute("errorMessage", errorMessage);
+				req.getRequestDispatcher("/_view/ChangeUsername.jsp").forward(req, resp);
+			}
+			else{
+				
+				errorMessage = "Incorrect Username or Password";
+				req.setAttribute("errorMessage", errorMessage);
+				req.getRequestDispatcher("/_view/ChangeUsername.jsp").forward(req, resp);
+			}
+		}
+		//otherwise, print an error message
+	}
 }
